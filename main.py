5151e95b4c3f60bf542b3a3d6f29491caa22fea5
Колесников Василий <kolesnikovasiliy@yandex.ru>
2022-04-12 15:28:35 +0300

    Проверил сервер

---
+++ main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 12 15:09:48 2022
+
+@author: 
+"""
+
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
